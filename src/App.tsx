eb2978c21186dd2921c5b97cd3462071c7adbae6
Andreazza <enrique0801@gmail.com>
2026-01-15 12:52:34 -0300

    Cadastro/login

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Footer from "./components/footer/Footer";
 import Navbar from "./components/navbar/Navbar";
 import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Cadastro from "./pages/cadastro/Cadastro";
 
 function App() {
   return (
     <>
-    <Navbar/>
-    <Home />
-    <Footer/>
+    <BrowserRouter>
+     <Navbar />
+        <Routes>
+          <Route path="/home" element={<Home />}/>
+          <Route path="/login" element={<Login />}/>
+          <Route path="/" element={<Cadastro />} />
+        </Routes>
+     <Footer/>
+    </BrowserRouter>
+    
     </>
   );
 }
--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -2,25 +2,31 @@ import { FacebookLogoIcon, InstagramLogoIcon, LinkedinLogoIcon } from "@phosphor
 
 function Footer() {
     
-    const data  = new Date().getFullYear()
+    const data = new Date().getFullYear()
 
     return(
         <>
           <div className="flex justify-center bg-indigo-900 text-white">
             <div className="container flex flex-col items-center py-4">
-                <p className="text-x1 font-bold">
+                <p className="text-xl font-bold">
                     Blog Pessoal Generation | Copyright: {data}
                 </p>
                 
                 <p className="text-lg">Acesse nossas redes sociais</p>
                 <div className="flex gap-2">
-                    <LinkedinLogoIcon size={48} weight="bold"/>
-                    <InstagramLogoIcon size={48} weight="bold"/>
-                    <FacebookLogoIcon size={48} weight="bold"/>
+                    <a href="https://www.linkedin.com" target="_blank" rel="noopener noreferrer">
+                        <LinkedinLogoIcon size={48} weight="bold"/>
+                    </a>
+                    <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
+                        <InstagramLogoIcon size={48} weight="bold"/>
+                    </a>
+                    <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
+                        <FacebookLogoIcon size={48} weight="bold"/>
+                    </a>
                 </div>
             </div>
           </div>
         </>
     )
 }
-export default Footer 
+export default Footer
