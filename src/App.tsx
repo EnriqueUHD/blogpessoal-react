cb37d9570a42229be8d69fc0e4e62d634fbdd864
Andreazza <enrique0801@gmail.com>
2026-01-22 21:40:54 -0300

    alertas personalizados

--- src/App.tsx
+++ src/App.tsx
@@ -12,30 +12,33 @@ import ListaPostagens from "./components/postagem/listapostagens/ListaPostagens"
 import FormPostagem from "./components/postagem/formpostagem/FormPostagem";
 import DeletarPostagem from "./components/postagem/deletarpostagem/DeletarPostagem";
 import Perfil from "./pages/perfil/Perfil";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
 
 function App() {
   return (
     <>
-    <AuthProvider>
-    <BrowserRouter>
-     <Navbar />
-        <Routes>
-          <Route path="/" element={<Login />}/>
-          <Route path="/home" element={<Home />}/>
-          <Route path="/cadastro" element={<Cadastro />} />
-          <Route path="/temas" element={<ListaTemas />} />
-          <Route path="/cadastrartema" element={<FormTema />} />
-          <Route path="/editartema/:id" element={<FormTema />} />
-          <Route path="/deletartema/:id" element={<DeletarTema />} />
-          <Route path="/postagens" element={<ListaPostagens />} />
-          <Route path="/cadastrarpostagem" element={<FormPostagem />} />
-          <Route path="/editarpostagem/:id" element={<FormPostagem />} />
-          <Route path="/deletarpostagem/:id" element={<DeletarPostagem />} />
-          <Route path="/perfil" element={<Perfil />} />
-        </Routes>
-     <Footer/>
-    </BrowserRouter>
-    </AuthProvider>
+      <AuthProvider>
+        <ToastContainer />
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/cadastro" element={<Cadastro />} />
+            <Route path="/temas" element={<ListaTemas />} />
+            <Route path="/cadastrartema" element={<FormTema />} />
+            <Route path="/editartema/:id" element={<FormTema />} />
+            <Route path="/deletartema/:id" element={<DeletarTema />} />
+            <Route path="/postagens" element={<ListaPostagens />} />
+            <Route path="/cadastrarpostagem" element={<FormPostagem />} />
+            <Route path="/editarpostagem/:id" element={<FormPostagem />} />
+            <Route path="/deletarpostagem/:id" element={<DeletarPostagem />} />
+            <Route path="/perfil" element={<Perfil />} />
+          </Routes>
+          <Footer />
+        </BrowserRouter>
+      </AuthProvider>
     </>
   );
 }
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,38 +1,43 @@
-import { useContext } from "react";
-import { Link, useNavigate } from "react-router-dom"
+import { useContext, type ReactNode } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { AuthContext } from "../../contexts/AuthContext";
+import { ToastAlerta } from "../../utils/ToastAlerta";
 
 function Navbar() {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
+  const { usuario, handleLogout } = useContext(AuthContext);
 
-    const {handleLogout} = useContext(AuthContext)
+  function logout() {
+    handleLogout();
+    ToastAlerta('O Usuário foi desconectado com sucesso!', 'info');
+    navigate('/');
+  }
 
-    function logout(){
-        handleLogout()
-        alert('O Usuário foidesconectado com sucesso!')
-        navigate('/')
-    }
+  let component: ReactNode;
 
-
-    return (
-        <>
-        <div className="w-full flex justify-center py-4
-                        bg-indigo-900 text-white">
-            <div className="container flex justify-between text-lg mx-8">
-                <Link to={"/home"} className="text-2xl font-bold">Blog Pessoal</Link>
-
-                <div className="flex gap-4">
-                    <Link to='/postagens' className='hover:underline'>Postagens</Link>
-                    <Link to="/temas" className="hover:underline">Temas</Link>
-                    <Link to="/cadastrartema" className="hover:underline">Cadastrar tema </Link>
-                    <Link to='/perfil' className='hover:underline'>Perfil</Link>
-                    <Link to="" onClick={logout} className="hover:underline">Sair</Link>
-                </div>
-            </div>
+  if (usuario.token !== "") {
+    component = (
+      <div className='w-full flex justify-center py-4 bg-indigo-900 text-white'>
+        <div className='container flex justify-between text-lg mx-8'>
+          <Link to='/' className='text-2xl font-bold'>Blog Pessoal</Link>
+          <div className='flex gap-4'>
+            <Link to='/postagens' className='hover:underline'>Postagens</Link>
+            <Link to='/temas' className='hover:underline'>Temas</Link>
+            <Link to='/cadastrartema' className='hover:underline'>Cadastrar tema</Link>
+            <Link to='/perfil' className='hover:underline'>Perfil</Link>
+            <Link to='' onClick={logout} className='hover:underline'>Sair</Link>
+          </div>
         </div>
+      </div>
+    );
+  }
+
+  return (
+    <>
+      { component }
     </>
-    )
+  );
 }
 
-export default Navbar
+export default Navbar;
